0e4155294ceccf149f328f5c3dcce7e389a4ab8b
AilexGPT <148038194+Einfachalf@users.noreply.github.com>
2023-11-24 20:21:32 +0100

    fgfg

--- config/index.ts
+++ config/index.ts
@@ -4,15 +4,16 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+  description: '',  // Fügen Sie hier Ihre App-Beschreibung ein
+  copyright: '',    // Fügen Sie hier Urheberrechtsinformationen ein
+  privacy_policy: '',  // Fügen Sie hier die Datenschutzrichtlinie ein
+  default_language: 'de'  // Standardsprache auf Deutsch gesetzt
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const isShowPrompt = true
+export const promptTemplate = ''  // Sie können hier eine Vorlage für Eingabeaufforderungen einstellen
 
+}
 export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
